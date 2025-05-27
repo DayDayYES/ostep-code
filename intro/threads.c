260090e42aa520ad9e6020219e78a8d6de529c61
DayDayYES <2412492703@qq.com>
2025-05-27 21:41:17 +0800

    new

--- intro/threads.c
+++ intro/threads.c
@@ -8,16 +8,16 @@ int loops;
 
 void *worker(void *arg) {
     int i;
-    for (i = 0; i < loops; i++) {
-	counter++;
+        for (i = 0; i < loops; i++) {
+        counter++;
     }
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) { 
-	fprintf(stderr, "usage: threads <loops>\n"); 
-	exit(1); 
+        fprintf(stderr, "usage: threads <loops>\n"); 
+        exit(1); 
     } 
     loops = atoi(argv[1]);
     pthread_t p1, p2;
